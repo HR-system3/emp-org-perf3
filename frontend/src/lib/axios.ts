5b7ac4e9065555a065e7d96817c297c21674041c
OmarElashry63 <omar.elashry@student.giu-uni.de>
2025-12-13 23:30:57 +0200

    updated axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,8 +1,13 @@
 // ./src/lib/axios.ts
+console.log("NEXT_PUBLIC_API_URL =", process.env.NEXT_PUBLIC_API_URL);
 
 import axios from "axios";
 
+const baseURL =
+  process.env.NEXT_PUBLIC_API_URL || "https://emp-org-perf3.onrender.com";
+
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  withCredentials: true, // keep if you use cookies auth
+  baseURL,
+  withCredentials: true,
 });
+
