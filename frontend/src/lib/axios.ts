c56a0b7b9e0f2ed3d763dd0cb240bd790f505d4f
OmarElashry63 <omar.elashry@student.giu-uni.de>
2025-12-14 00:18:00 +0200

    updated

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -4,10 +4,10 @@ import axios from "axios";
 const baseURL =
   process.env.NEXT_PUBLIC_API_URL || "https://emp-org-perf3.onrender.com";
 
-console.log("NEXT_PUBLIC_API_URL =", process.env.NEXT_PUBLIC_API_URL);
-console.log("API BASE URL =", baseURL);
-
-export const api = axios.create({
+const api = axios.create({
   baseURL,
   withCredentials: true,
 });
+
+export { api };
+export default api;
